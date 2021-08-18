d783e828a1be767407bfe4723bb8aba115bfbd04
derek <derekvaughan51@gmail.com>
2021-08-17 21:24:43 -0700

    Initial version, testing host blocking.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AntiProcrastinator
 {
@@ -6,7 +7,28 @@ namespace AntiProcrastinator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const String FilePath = @"C:\Windows\System32\drivers\etc\hosts";
+
+            Console.WriteLine("Let's get to work.");
+
+            StartWebsiteBlocker(FilePath);
+        }
+
+        static void StartWebsiteBlocker(String FilePath)
+        {
+            Console.WriteLine("blocking...");
+
+            StreamWriter sw = new StreamWriter(FilePath, true);
+            String website = "\n 127.0.0.0 www.solitaired.com\n 127.0.0.0 https://www.solitaired.com\n 127.0.0.0 solitaired.com\n 127.0.0.0 https://solitaired.com\n 127.0.0.0 http://solitaired.com\n 127.0.0.0 https://solitaired.com/";
+            sw.Write(website);
+            sw.Close();
+            Console.WriteLine("Website has been blocked");
+        }
+
+        static void EndWebsiteBlocker(String FilePath)
+        {
+            Console.WriteLine("Unblocking...");
+            Console.WriteLine(FilePath);
         }
     }
 }
