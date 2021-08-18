65a222b019f91859e78e0790661fd42656b658e0
derek <derekvaughan51@gmail.com>
2021-08-17 20:39:50 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AntiProcrastinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
